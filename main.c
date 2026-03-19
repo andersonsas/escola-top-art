6d987e964ceb29db7aba080b987207800f4e2bd8
anderson <anderson_under@hotmail.com>
2026-03-19 02:47:47 -0400

    Inserção simples

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #define LIMPAR_TELA system("cls")
 /* ===================== CONSTANTES ===================== */
-#define MAX_CPF 15
+#define MAX_CPF 12
 #define MAX_NOME 100
 #define MAX_CODIGO 10
+
+#define MAX_DISCENTES 100
+#define MAX_CURSOS 30
+#define MAX_TURMAS 100
 
 #define ARQ_DISCENTES "discentes.txt"
 #define ARQ_CURSOS "cursos.txt"
@@ -35,7 +39,17 @@ typedef struct {
     int ano;
     float nota;
     int hora_participacao;
-} Turmas;
+} Turma;
+
+/* ========== ARRAY GLOBAIS ========== */
+Discente discentes[MAX_DISCENTES];
+int total_discentes = 0;
+
+Curso cursos[MAX_CURSOS];
+int total_cursos = 0;
+
+Turma turmas[MAX_TURMAS];
+int total_turmas = 0;
 
 /* ================= FUNCOES AUXILIARES ================= */
 void limpar_buffer() {
@@ -50,9 +64,7 @@ void pausar() {
     getchar();
 }
 
-void limpar_tela() {
-    system("cls");
-}
+void limpar_tela() { system("cls"); }
 
 void cabecalho(const char *titulo) {
     limpar_tela();
@@ -60,13 +72,13 @@ void cabecalho(const char *titulo) {
     printf("\n");
 }
 
-/* =============== VALIDACOES =========================   */
+/* =============== VALIDACOES ========================= */
 int cpf_valido(const char *cpf) {
     int i;
     if (strlen(cpf) != 11) return 0;
 
     for (i = 0; i < 11; i++) {
-        if (!isdigit((unsigned char)cpf[i])) {
+        if (!isdigit(cpf[i])) {
             return 0;
         }
     }
@@ -75,76 +87,86 @@ int cpf_valido(const char *cpf) {
 
 /* ===================== LEITURA SEGURA ===================== */
 
-void ler_string(const char *legenda, char *dest, int max) {
+// ler entrada estabelecendo uma legenda e um limite de números
+void ler_string(const char legenda[], char dest[], int max) {
     printf("%s", legenda);
     fgets(dest, max, stdin);
     dest[strcspn(dest, "\n")] = '\0';
 }
 
-int ler_inteiro(const char *legenda, int min, int max_val) {
-    int val;
+int ler_inteiro(const char legenda[], int min, int max_val) {
+    int valor;
     char buf[50];
     while (1) {
         printf("%s", legenda);
         fgets(buf, sizeof(buf), stdin);
-        if (sscanf(buf, "%d", &val) == 1 && val >= min && val <= max_val) return val;
+        if (sscanf(buf, "%d", &valor) == 1 && valor >= min && valor <= max_val)
+            return valor;
         printf("  [!] Valor inválido. Digite entre %d e %d.\n", min, max_val);
     }
 }
-
-
 
 /* ===================== DISCENTES - ARQUIVO ===================== */
 int contar_discentes() {
     FILE *f = fopen(ARQ_DISCENTES, "r");
     if (!f) return 0;
+
     int cont = 0;
     char linha[200];
     while (fgets(linha, sizeof(linha), f)) cont++;
+
     fclose(f);
     return cont;
 }
 
-Discente *carregar_discentes(int *total) {
-    *total = contar_discentes();
-    if (*total == 0) return NULL;
-    Discente *lista = (Discente *)malloc(*total * sizeof(Discente));
-    if (!lista) { *total = 0; return NULL; }
+void carregar_discentes() {
     FILE *f = fopen(ARQ_DISCENTES, "r");
-    if (!f) { free(lista); *total = 0; return NULL; }
-    int i = 0;
-    while (i < *total && fscanf(f, "%11[^|]|%99[^|]|%d\n",
-        lista[i].cpf, lista[i].nome, &lista[i].idade) == 3)
-        i++;
-    *total = i;
+    if (!f) { printf("Error ao carregar"); pausar(); return; }
+
+    total_discentes = 0;
+    while (total_discentes < MAX_DISCENTES &&
+        fscanf(f,
+            "%14[^|]|%99[^|]|%d\n",
+            discentes[total_discentes].cpf,
+            discentes[total_discentes].nome,
+            &discentes[total_discentes].idade) == 3) {
+        total_discentes++;
+    }
     fclose(f);
-    return lista;
 }
 
-void salvar_discentes(Discente *lista, int total) {
+void salvar_discentes() {
     FILE *f = fopen(ARQ_DISCENTES, "w");
     if (!f) { printf("  [!] Erro ao salvar discentes.\n"); return; }
+
     int i;
-    for (i = 0; i < total; i++)
-        fprintf(f, "%s|%s|%d\n", lista[i].cpf, lista[i].nome, lista[i].idade);
+    for (i = 0; i < total_discentes; i++)
+        fprintf(f, "%s|%s|%d\n",
+            discentes[i].cpf,
+            discentes[i].nome,
+            discentes[i].idade);
     fclose(f);
 }
 
 int cpf_discente_existe(const char *cpf) {
-    int total;
-    Discente *lista = carregar_discentes(&total);
+    //verificar depois
+    /* int total;
+    Discente lista[] = carregar_discentes(&total);
     int encontrado = 0;
     int i;
+
     for (i = 0; i < total; i++) {
         if (strcmp(lista[i].cpf, cpf) == 0) { encontrado = 1; break; }
     }
+
     free(lista);
-    return encontrado;
+    return encontrado; */
 }
 
 /* ===================== FUNÇÕES BUSCA AUXILIAR ===================== */
 
-int buscar_discente_por_cpf(Discente *lista, int total, const char *cpf, Discente *dest) {
+int buscar_discente_por_cpf(Discente *lista, int total, const char *cpf,
+    Discente *dest) {
     int i;
     for (i = 0; i < total; i++) {
         if (strcmp(lista[i].cpf, cpf) == 0) {
@@ -158,29 +180,27 @@ int buscar_discente_por_cpf(Discente *lista, int total, const char *cpf, Discent
 /* ============== MENU DISCENTE ================*/
 void inserir_discente() {
     cabecalho("DISCENTES > INSERIR");
-    Discente novo;
 
+    // formulário
+    Discente novo;
     ler_string("  CPF (apenas dígitos): ", novo.cpf, MAX_CPF);
-    if (!cpf_valido(novo.cpf)) { printf("  [!] CPF inválido.\n"); pausar(); return; }
-    if (cpf_discente_existe(novo.cpf)) { printf("  [!] CPF já cadastrado.\n"); pausar(); return; }
+    limpar_buffer();
 
     ler_string("  Nome: ", novo.nome, MAX_NOME);
-    if (strlen(novo.nome) < 3) { printf("  [!] Nome muito curto.\n"); pausar(); return; }
+    if (strlen(novo.nome) < 3) {
+        printf("  [!] Nome muito curto.\n");
+        pausar(); return;
+    }
 
     novo.idade = ler_inteiro("  Idade: ", 5, 120);
 
-    int total;
-    Discente *lista = carregar_discentes(&total);
-    Discente *nova_lista = (Discente *)realloc(lista, (total + 1) * sizeof(Discente));
-    if (!nova_lista) { printf("  [!] Erro de memória.\n"); free(lista); pausar(); return; }
-    lista = nova_lista;
-    lista[total] = novo;
-    salvar_discentes(lista, total + 1);
-    free(lista);
+    discentes[total_discentes] = novo;
+    total_discentes++;
+    salvar_discentes();
+
     printf("\n  [OK] Discente cadastrado com sucesso!\n");
     pausar();
 }
-
 
 void menu_discentes() {
     int op;
@@ -193,10 +213,12 @@ void menu_discentes() {
         printf("  [0] Voltar\n\n");
         op = ler_inteiro("  Opcao: ", 0, 4);
         switch (op) {
-            case 1: inserir_discente(); break;
-                //case 2: editar_discente(); break; //TODO
-                //case 3: excluir_discente(); break;
-                //case 4: pesquisar_discente(); break;
+            case 1:
+                inserir_discente();
+                break;
+                // case 2: editar_discente(); break; //TODO
+                // case 3: excluir_discente(); break;
+                // case 4: pesquisar_discente(); break;
         }
     } while (op != 0);
 }
@@ -215,10 +237,18 @@ void menu_principal() {
         op = ler_inteiro("Opcao: ", 0, 4);
 
         switch (op) {
-            case 1: menu_discentes(); break;
-            case 2: printf("Vc esta na op2"); break;
-            case 3: printf("Vc esta na op3"); break;
-            case 4: printf("Vc esta na op4"); break;
+            case 1:
+                menu_discentes();
+                break;
+            case 2:
+                printf("Vc esta na op2");
+                break;
+            case 3:
+                printf("Vc esta na op3");
+                break;
+            case 4:
+                printf("Vc esta na op4");
+                break;
             case 0:
                 cabecalho("ATÉ LOGO!");
                 printf("  Sistema encerrado.\n\n");
@@ -229,6 +259,8 @@ void menu_principal() {
 
 int main() {
     system("chcp 65001 > nul");
+    carregar_discentes();
+
     menu_principal();
 
     return 0;
